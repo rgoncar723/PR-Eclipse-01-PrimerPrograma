c5e080b151dc21465d81f133ea1fcd4e356bcc15
Raquel Gonzalez Carrillo <rgoncar723@g.educaand.es>
2025-10-03 14:38:53 +0200

    Add files via upload

---
+++ PrimerPrograma/src/tema2_eclipse/HelloEclipse.java
@@ -0,0 +1,30 @@
+package tema2_eclipse;
+
+public class HelloEclipse {
+
+	public static void main(String[] args) {
+		// Comentario de una línea: primer programa
+		System.out.println("¡Mi primer programa!");
+		System.out.println("Reglas básicas de Java:");
+		System.out.println("1) Distingue MAYÚSCULAS y minúsculas");
+		System.out.println("2) Cada instrucción termina con ;");
+		// Este programa imprime mensajes en consola
+		/*
+		  Autor: Raquel Gonzalez Carrillo
+		 *Fecha: 3/10/25
+		 */
+		
+		int x = 3;
+		System.out.print("Este código no está");
+		System.out.print(" muy bien ");
+		System.out.print("tabulado");
+		x = 7;
+		System.out.println();
+		
+		int contadorMensajes= 0;
+		contadorMensajes++;
+		System.out.println("Mensajes impresos: "+ contadorMensajes);
+		
+	}
+
+}
